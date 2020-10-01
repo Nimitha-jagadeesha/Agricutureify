c2800125bd83154949930a910205022ecbe6ac7f
Nimitha-jagadeesha <nimitha1jagadeesha@gmail.com>
2020-10-01 21:47:59 +0530

    Initial-commit

---
+++ agro/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import './App.css';
+import { BrowserRouter } from "react-router-dom";
+import Header from './components/Header'
+function App() {
+  return (
+    <BrowserRouter>
+    <div className="App">
+      <Header />
+    </div>
+  </BrowserRouter>
+  );
+}
+
+export default App;
