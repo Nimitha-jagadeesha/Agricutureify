720e7c86171a9c41963f53a3d68ebf57dd423b1e
roopashree375 <62794413+roopashree375@users.noreply.github.com>
2020-10-07 23:50:58 +0530

    home modified

--- agro/src/components/Home.js
+++ agro/src/components/Home.js
@@ -79,15 +79,14 @@ const Home = (props) => {
             <div className="col">
               <h1>Agro Connect</h1>
               <p>
-                The main aim of this website is to educate farmers and solve all
-                their problems by using technology. Even though technology has
-                gone too far beyond, technology is failing to reach
-                everyone,especially farmers.So to provide an alternative avenue
-                to sell their produce .So here is an platform which provides
-                farmers platform to directly contact the buyer, so that there is
-                no middle man making the profit. So in this websitee we are
-                providing various buyers details whih includes their contact
-                number so that farmers will be benefited.
+              Over the years, the system has created several layers of intermediaries, lengthening the supply chain and increasing the opportunity for cartels to form, which in turn drive prices down for farmers. Farmers earn only 15 to 20% of the market price the consumer pays.The middle man be it the wholesalers or retailers gobble up the remaining profit margin
+The main reasons for this exploitation experienced by farmers are , lack of infrastructure for farmers to store their produce to bargain their prices, they cannot organise them selves as one force, The small holdings is another drawback for farmers to demand the right price.
+The best way is to give farmers the power to sell  or giving them an opportunity which connects them to consumers directly rather than middle man minting farmers money.
+ If the consumer knows where to find the farm produce he may buy directly from the farmer himself.
+              </p>
+              <p>
+              We connect farmers to end consumers direct and help each other trade the grains at much better rates. It ensure that farmers get the fair rate of their grains. More than market and also for consumers it ensure that they get best quality grains at best price as compared to what they buy from super markets.
+Also If you're a farmer and want to get your crops, food grains at better rates than local mandi/traders we are here to help you.
               </p>
             </div>
           </div>
