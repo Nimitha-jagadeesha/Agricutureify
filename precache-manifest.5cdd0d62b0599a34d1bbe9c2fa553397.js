78dd54e145759083c142c88a9f05fd92b00a75f7
Nimitha-jagadeesha <nimitha1jagadeesha@gmail.com>
2020-10-25 20:40:33 +0530

    Updates

---
+++ precache-manifest.5cdd0d62b0599a34d1bbe9c2fa553397.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dde6eb8082318e8074ffba3aee60371d",
+    "url": "/Agricutureify/index.html"
+  },
+  {
+    "revision": "851ae3c766fab2965c06",
+    "url": "/Agricutureify/static/css/2.2d03384e.chunk.css"
+  },
+  {
+    "revision": "d028a090effeb69dde3b",
+    "url": "/Agricutureify/static/css/main.d83b1ea7.chunk.css"
+  },
+  {
+    "revision": "851ae3c766fab2965c06",
+    "url": "/Agricutureify/static/js/2.ce738ba2.chunk.js"
+  },
+  {
+    "revision": "14060d5403b5cb599a71704f7b391b06",
+    "url": "/Agricutureify/static/js/2.ce738ba2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d028a090effeb69dde3b",
+    "url": "/Agricutureify/static/js/main.232ca2ab.chunk.js"
+  },
+  {
+    "revision": "e2bf5e8d6361b7f6abba",
+    "url": "/Agricutureify/static/js/runtime-main.045e1a97.js"
+  },
+  {
+    "revision": "4197224f78215701b5a9661d6ca0f957",
+    "url": "/Agricutureify/static/media/AboutUs.4197224f.jpg"
+  },
+  {
+    "revision": "9a217c274feaa507aed3d993064a4dde",
+    "url": "/Agricutureify/static/media/contact.9a217c27.jpg"
+  },
+  {
+    "revision": "0d8e14ca5948547510418d4cab08e391",
+    "url": "/Agricutureify/static/media/download.0d8e14ca.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Agricutureify/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Agricutureify/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Agricutureify/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Agricutureify/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Agricutureify/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b73207059533fd3b49386ce428921a60",
+    "url": "/Agricutureify/static/media/nature.b7320705.jpg"
+  },
+  {
+    "revision": "de42acd308750770fc7c5e4005d804a0",
+    "url": "/Agricutureify/static/media/organic.de42acd3.jpg"
+  },
+  {
+    "revision": "d46d810bf390aa5a9e092d5f5a1679a4",
+    "url": "/Agricutureify/static/media/s2.d46d810b.jpeg"
+  },
+  {
+    "revision": "b984eb165d4a5444626b1903e5d5a529",
+    "url": "/Agricutureify/static/media/s3.b984eb16.jpeg"
+  },
+  {
+    "revision": "421fe2db563b85ccabf9a58a6ec5ad96",
+    "url": "/Agricutureify/static/media/s4.421fe2db.jpeg"
+  }
+]);
